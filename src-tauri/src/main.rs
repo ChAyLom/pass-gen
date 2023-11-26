575e4165867ddc3feb252913f138111bb36129f4
Ilya Chavlytko <ilya_chavlytko@mail.ru>
2023-11-26 22:54:03 +0700

    add auto open devtools in develop mode

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
 use tauri_plugin_window_state::{AppHandleExt, StateFlags};
 
 #[tauri::command]
@@ -11,6 +12,11 @@ fn close_app(app_handle: tauri::AppHandle) {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            app.get_window("main").unwrap().open_devtools();
+            Ok(())
+        })
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .invoke_handler(tauri::generate_handler![close_app])
         .run(tauri::generate_context!())
