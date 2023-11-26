7065478a285db22c95b77fa15a0aba818da22ae5
Ilya Chavlytko <ilya_chavlytko@mail.ru>
2023-11-26 22:53:42 +0700

    add run hidden and show after load

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 import { FocusStyleManager } from '@blueprintjs/core';
+import { appWindow } from '@tauri-apps/api/window';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
+appWindow.show();
 FocusStyleManager.onlyShowFocusOnTabs();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
